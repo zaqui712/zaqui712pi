b12fdc843a4b493622c25d7c544daade4ef16f93
zaqui712 <a21192@alunos.ipca.pt>
2020-10-30 10:56:17 +0000

    1stcommit

---
+++ hoje.c
@@ -0,0 +1,22 @@
+/**
+ * @file aula.c
+ * @author Ezequiel (a21192@edomain.com)
+ * @brief  Aula basica
+ * @version 0.1
+ * @date 2020-10-26
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+printf("AULA");
+getchar();
+
+return 0;
+
+}
